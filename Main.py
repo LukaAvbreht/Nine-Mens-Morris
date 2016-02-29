8b0104256453e430fdf1d80abc5c162176707439
AvbrehtL14 <l.avbreht@gmail.com>
2016-02-29 10:03:13 +0100

    dodane crte na canvas, doddana logika igre

--- Main.py
+++ Main.py
@@ -6,17 +6,42 @@ class tkmlin:
         self.master = master
         self.master.minsize(width=700, height=800)
 
-        plosca = Frame(master,width=700,height=700)
+        #Igralna plosca
+        plosca = Canvas(master,width=700,height=700)
         plosca.grid(row = 1, column = 0, rowspan = 7, columnspan = 7, sticky=N+S+E+W)
 
+        #crte za igralno plosco
+        plosca.create_line(50,50,650,50)
+        plosca.create_line(50,650,650,650)
+        plosca.create_line(50,50,50,650)
+        plosca.create_line(650,50,650,650)
 
+        plosca.create_line(150,150,550,150)
+        plosca.create_line(550,150,550,550)
+        plosca.create_line(150,150,150,550)
+        plosca.create_line(150,550,550,550)
+
+        plosca.create_line(250,250,450,250)
+        plosca.create_line(450,250,450,450)
+        plosca.create_line(250,250,250,450)
+        plosca.create_line(250,450,450,450)
+
+        plosca.create_line(350,50,350,250)
+        plosca.create_line(350,450,350,650)
+        plosca.create_line(50,350,250,350)
+        plosca.create_line(450,350,650,350)
+
+        #Gumpki namenjeni za igro
+
+        #generira gumbe ki niso povezani z igro
         gumb_novaigra = Button(master, text="Nova igra", command= self.newgame, height=2)
         gumb_novaigra.grid(row=0,column=0,sticky=N+S+E+W)
 
     def newgame(self):
         return "to do"
 
-root = Tk()
-root.wm_title('Nine Man\'s Morris')
-okno = tkmlin(root)
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    root.wm_title('Nine Man\'s Morris')
+    okno = tkmlin(root)
+    root.mainloop()
---
+++ igra.py
@@ -0,0 +1,13 @@
+__author__ = 'LukaAvbreht,SamoKralj'
+
+class Igra():
+    """Program namenjen logiki in pravilom igre"""
+
+    def __init__(self):
+        self.plosca =[[None,None,None,None,None,None,None],
+                      [None,None,None,None,None,None,None],
+                      [None,None,None,None,None,None,None],
+                      [None,None,None,None,None,None,None],
+                      [None,None,None,None,None,None,None],
+                      [None,None,None,None,None,None,None],
+                      [None,None,None,None,None,None,None]]
